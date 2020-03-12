cd160b19b7e86a8da7a5d250db855163b1e7a22c
sergiooli1997 <gio.lozol@outlook.com>
2020-03-12 11:39:16 -0600

    V2.0 restricciones

--- lector-escritor.py
+++ lector-escritor.py
@@ -10,29 +10,28 @@ def lector(lock, barrier):
     print(threading.current_thread().name,
           'Esperando en la barrera con {} lectores más'.format(barrier.n_waiting))
     worker_id = barrier.wait()
-    logging.debug('Intento acceder a la BD.')
+    logging.debug('Intento acceder al dato.')
     lock.acquire()
     try:
-        logging.debug('Accedio a la BD.')
+        logging.debug('Accedio al dato.')
         logging.debug('Lee {}'.format(dato))
         time.sleep(3.0)
     finally:
         lock.release()
-        logging.debug('Dejo de usar la BD.')
 
 
-def escritor(cond):
+def escritor(cond, n):
     global dato
     logging.debug('Intento acceder a la BD.')
     lock.acquire()
     try:
         logging.debug('Accedio a la BD.')
-        dato = 1
-        logging.debug('Modifico la BD. Dato = {}'.format(dato))
+        dato = n
+        logging.debug('Modifico el dato = {}'.format(dato))
         time.sleep(3.0)
     finally:
         lock.release()
-        logging.debug('Dejo de usar la BD.')
+        logging.debug('Dejo de modificar el dato.')
 
 
 logging.basicConfig(level=logging.DEBUG,
@@ -41,8 +40,8 @@ logging.basicConfig(level=logging.DEBUG,
 
 lock = threading.Lock()
 barrier = threading.Barrier(2)
-escritor1 = threading.Thread(target=escritor, args=(lock,), name='Escritor1', )
-escritor2 = threading.Thread(target=escritor, args=(lock,), name='Escritor2', )
+escritor1 = threading.Thread(target=escritor, args=(lock, 1), name='Escritor1', )
+escritor2 = threading.Thread(target=escritor, args=(lock, 2), name='Escritor2', )
 lector1 = threading.Thread(target=lector, args=(lock, barrier,), name='Lector1', )
 lector2 = threading.Thread(target=lector, args=(lock, barrier,), name='Lector2', )
 escritor1.start()
